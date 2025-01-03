93516c38e02f7f7e37a4b105c92a4bc2a08b6a3a
Mathias Ilskens <mathias@significo.com>
2025-01-03 21:00:49 +0100

    add animations for progress and done

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
+let isInProgress = false;
+
 document.getElementById("check-all").addEventListener("click", () => {
+  if (isInProgress) {
+    return;
+  }
+  isInProgress = true;
+  showLoadingAnimation();
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs[0]?.id) {
       chrome.scripting.executeScript({
@@ -8,6 +15,39 @@ document.getElementById("check-all").addEventListener("click", () => {
     }
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.done) {
+      showDone();
+      setTimeout(() => {
+        isInProgress = false;
+        showCta();
+      }, 9999);
+    }
+  }
+);
+
+function showLoadingAnimation() {
+  document.getElementById("btn-label").style.display = 'none';
+  document.getElementById("btn-label-done").style.display = 'none';
+  document.getElementById("loading-animation").style.display = 'block';
+  document.getElementById("done-icon").style.display = 'none';
+}
+
+function showDone() {
+  document.getElementById("btn-label").style.display = 'none';
+  document.getElementById("btn-label-done").style.display = 'inline';
+  document.getElementById("loading-animation").style.display = 'none';
+  document.getElementById("done-icon").style.display = 'block';
+}
+
+function showCta() {
+  document.getElementById("btn-label").style.display = 'inline';
+  document.getElementById("btn-label-done").style.display = 'none';
+  document.getElementById("loading-animation").style.display = 'none';
+  document.getElementById("done-icon").style.display = 'none';
+}
 
 function checkAllCheckboxes() {
   const checkboxes = document.querySelectorAll('input.task-list-item-checkbox:not(:checked)');
@@ -20,5 +60,7 @@ function checkAllCheckboxes() {
 
   if (numUnchecked > 0) {
     window.setTimeout(checkAllCheckboxes, 10);
+  } else {
+    chrome.runtime.sendMessage({done: true});
   }
 }
